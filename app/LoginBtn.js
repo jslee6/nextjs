fa110d7cc1206e27503f51b20f8d2932722372e4
이진석 <gkrryrwkk6@naver.com>
2024-05-21 11:12:24 +0900

    깃허브 로그인 로그아웃

--- app/LoginBtn.js
+++ app/LoginBtn.js
@@ -1,9 +1,12 @@
 'use client'
 
-'use client';
 import { signIn } from 'next-auth/react'
+
 
 export default function LoginBtn() {
   return <button onClick={() => { signIn() }}>로그인</button>
 } 
+
+
+  
 
