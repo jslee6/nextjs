cae942e7effdae1a43db06940815b945803a8daa
이진석 <gkrryrwkk6@naver.com>
2024-06-11 00:11:03 +0900

    commit

---
+++ app/ptest/page.js
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default async function get({ }) {
+    try {
+        const users = await prisma.user.findMany();
+        console.log(users.map(user => user.id)); // 사용자의 id만 콘솔에 출력
+        // console.log(users.map(user)); // 전체 출력
+
+        return (
+            <div>
+                <h4>상세페이지임</h4>
+                {
+                users.map((user) => (
+                    <div key={user.id}>
+                        <h4>아이디: {user.id}, </h4>
+                        <h4>이메일: {user.email}</h4>
+                    </div>
+                ))
+                }
+                {/* 애로우 펑션 */}
+            </div>
+        );
+    } catch (error) {
+        console.error(error);
+        return (
+            <div>
+                <h4>데이터를 가져오는 중에 오류가 발생했습니다.</h4>
+            </div>
+        );
+    }
+}
---
+++ pages/api/user.js
@@ -0,0 +1,43 @@
+// pages/api/users.js
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function select(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const users = await prisma.user.findMany({
+        select: {
+          id: true,
+          email: true
+        }
+      });
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
+
+
+
+
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// export default async function handler(req, res) {
+//   if (req.method === 'GET') {
+//     try {
+//       const users = await prisma.user.findMany();
+//       res.status(200).json(users);
+//     } catch (error) {
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   } else {
+//     res.status(405).json({ error: 'Method Not Allowed' });
+//   }
+// } 기본 Select
+
