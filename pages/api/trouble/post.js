fe404f5902f3add9d2c418096c5748e365655bae
이진석 <gkrryrwkk6@naver.com>
2024-10-15 17:29:39 +0900

    troubleshooting2 업뎃

--- pages/api/trouble/post.js
+++ pages/api/trouble/post.js
@@ -30,36 +30,6 @@
 // /pages/api/trouble/post.js
 // 기존 date 타입과 datetime 타입 땜에 안됨
 
-// import { PrismaClient } from '@prisma/client';
-
-// const prisma = new PrismaClient();
-
-// export default async function handler(req, res) {
-//   if (req.method === 'POST') {
-//     // Log the incoming request body
-//     console.log('Request body:', req.body);
-
-//     try {
-//       // Create a new user in the database
-//       const trouble = await prisma.troubleshooting.create({
-//         data: req.body, // 요청 본문을 그대로 사용
-//       });
-
-//       //user? 를 ->   trouble
-
-//       // 리다이렉트 또는 성공 응답
-//       res.status(201).json(trouble);  // 성공적인 응답
-//     } catch (error) {
-//       console.error('Error creating user:', error);
-//       res.status(500).json({ error: 'Failed to create 트러블슈팅', details: error.message });
-//     }
-//   } else {
-//     res.status(405).json({ message: 'Method not allowed' });
-//   }
-// }
-
-
-
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -68,11 +38,11 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     // Log the incoming request body
     console.log('Request body:', req.body);
-    const context = { ...req.body, resolvedAt: new Date(req.body.resolvedAt) }
+
     try {
       // Create a new user in the database
       const trouble = await prisma.troubleshooting.create({
-        data: context, // 요청 본문을 그대로 사용
+        data: req.body, // 요청 본문을 그대로 사용
       });
 
       //user? 를 ->   trouble
@@ -87,3 +57,38 @@ export default async function handler(req, res) {
     res.status(405).json({ message: 'Method not allowed' });
   }
 }
+
+
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// export default async function handler(req, res) {
+//   if (req.method === 'POST') {
+//     // Log the incoming request body
+//     console.log('Request body:', req.body);
+
+//     // 날짜를 Date 객체로 변환하고 요청 본문을 병합합니다.
+//     const setTroubleData = {
+//       ...req.body,
+//       resolvedAt: new Date(req.body.resolvedAt),
+//       occurredAt: new Date(req.body.occurredAt),
+//     };
+
+//     try {
+//       // Create a new trouble entry in the database
+//       const trouble = await prisma.troubleshooting.create({
+//         data: setTroubleData, // 병합된 요청 본문을 사용
+//       });
+
+//       // 성공적인 응답
+//       res.status(201).json(trouble);
+//     } catch (error) {
+//       console.error('Error creating troubleshooting:', error);
+//       res.status(500).json({ error: 'Failed to create 트러블슈팅', details: error.message });
+//     }
+//   } else {
+//     res.status(405).json({ message: 'Method not allowed' });
+//   }
+// }
+
